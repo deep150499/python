26d3ef2d1ed2c036053760e9dbe47ca4c931c657
“deep150499” <deepnanavati@icloud.com>
2024-05-30 14:32:29 -0400

    variables

--- index.py
+++ index.py
@@ -1,4 +1,17 @@
-print("hello");
-print('Learning python');
+print("hello")
+print('Learning python')
 
-print('Hello',"How are you?");
+
+print('Hello',"How are you?")
+
+print(23)
+print(23+34)
+
+name = "John"
+print(name)
+
+age = 24
+print(age)
+
+print("My name is :", name)
+print("My age is :", age)
