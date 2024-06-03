80c64c85b6b8e546828e09c3d55ebed7d7eac618
“deep150499” <deepnanavati@icloud.com>
2024-06-03 15:18:20 -0400

    updated index

--- index.py
+++ index.py
@@ -152,6 +152,187 @@ b = 34
 
 # print("Both values are greater than :", (a>10) and (b>10))
 
+# print("Both values are greater than :", (a>10) or (b>60))
+
+# Name = input('Enter your name : ')
+# print('Welcome',Name)
+
+# name = (input('Enter your name:'))
+# print(type(name), 'Welcome', name)
+
+# age = int(input('Enter your age:'))
+# print(type(age) , 'Your age is',age)
+
+# marks = float(input('Enter your marks'))
+# print(type(marks), 'Your marks are', marks)
+
+# num1 = int(input('Enter number 1:'))
+# num2 = int(input('Enter number 2:'))
+
+print(num1 + num2)
+
+str1 = 'Name'
+len1 = len(str1)
+
+str2 = 'Age'
+len2 = len(str2)
+
+print(len1)
+print(len2)
+# print("hello")
+# print('Learning python')
+
+
+# print('Hello',"How are you?")
+
+# print(23)
+# print(23+34)
+
+name = "John"
+# print(name)
+
+age = 24
+# print(age)
+
+price = 56.90
+isTrue = True
+
+# print("My name is :", name)
+# print("My age is :", age)
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+# print(type(isTrue))
+
+first_Name = "Nishta"
+# print(first_Name)
+
+x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+x = "Python"
+y = "is"
+z = "awesome"
+# print(x, y, z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+# print(x + y + z)
+
+x = 5
+y = "John"
+# print(x + y)
+# Error as the date types of both the variables are different
+
+x = 5
+y = "John"
+# print(x, y)
+
+def greet(name):
+    print('Hello',name)
+
+# greet('Learner')
+# greet('David')
+# greet('How are you?')
+
+
+# print(not False)
+
+a = 60
+b = 34
+
+# print(not (a>b))
+
+# print("Both values are greater than :", (a>10) and (b>10))
+
+# print("Both values are greater than :", (a>10) or (b>60))
+
+# print('hello')
+# print('Learning python')
+
+
+# print('Hello','How are you?')
+
+# print(23)
+# print(23+34)
+
+name = 'John'
+print(name)
+
+age = 24
+print(age)
+
+price = 56.90
+isTrue = True
+
+# print("My name is :", name)
+# print("My age is :", age)
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+# print(type(isTrue))
+
+first_Name = 'Nishta'
+# print(first_Name)
+
+x, y, z = 'Orange', 'Banana', 'Cherry'
+# print(x)
+# print(y)
+# print(z)
+
+fruits = ['apple', 'banana', 'cherry']
+x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+x = 'Python'
+y = 'is'
+z = 'awesome'
+# print(x, y, z)
+
+x = 'Python '
+y = 'is '
+z = 'awesome'
+# print(x + y + z)
+
+x = 5
+y = 'John'
+# print(x + y)
+# Error as the date types of both the variables are different
+
+x = 5
+y = 'John'
+# print(x, y)
+
+def greet(name):
+    print('Hello',name)
+
+# greet('Learner')
+# greet('David')
+# greet('How are you?')
+
+
+# print(not False)
+
+a = 60
+b = 34
+
+# print(not (a>b))
+
+# print("Both values are greater than :", (a>10) and (b>10))
+
 <<<<<<< HEAD
 print("Both values are greater than :", (a>10) or (b>60))
 =======
@@ -173,17 +354,4 @@ print("Both values are greater than :", (a>10) or (b>60))
 num1 = int(input('Enter number 1:'))
 num2 = int(input('Enter number 2:'))
 
-<<<<<<< HEAD
 print(num1 + num2)
-=======
-print(num1 + num2)
-
-str1 = 'Name'
-len1 = len(str1)
-
-str2 = 'Age'
-len2 = len(str2)
-
-print(len1)
-print(len2)
->>>>>>> bd4e9d1 (dt)
